bdc029d806ed964f8399e199785f76a433a29bb6
Preetham14321 <79894297+Preetham14321@users.noreply.github.com>
2022-02-14 03:17:32 +0530

    Completed Task

--- src/component/Contactus.jsx
+++ src/component/Contactus.jsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/ostelloLogo.png";
+
+import { HiMenu } from "react-icons/hi";
+import { HiX } from "react-icons/hi";
 import { HiLocationMarker } from "react-icons/hi";
 import { HiPhone } from "react-icons/hi";
 import { HiOutlineMail } from "react-icons/hi";
@@ -11,59 +14,128 @@ import Footer from "./Footer";
 // import "./index.css";
 
 const Contactus = () => {
+  const [name, setName] = useState(false);
+  const [email, setEmail] = useState(false);
+  const [message, setMessage] = useState(false);
+  const [nameValue, setNameValue] = useState("");
+  const [emailValue, setEmailValue] = useState("");
+  const [messageValue, setMessageValue] = useState("");
+  const [showMenu, setShowMenu] = useState(false);
+
+  const submitHandler = () => {
+    nameValue.length < 1 ? setName(true) : setName(false);
+    emailValue.length < 1 ? setEmail(true) : setEmail(false);
+    messageValue.length < 1 ? setMessage(true) : setMessage(false);
+  };
+
   return (
     <>
-      <div className="container pl-28 pr-20 bg-gradient-to-b from-cyan-200 to-white h-96 absolute ">
+      <div className="container sm:p-10 md:p-0 md:pl-28 md:pr-20 bg-gradient-to-b from-cyan-200 to-white h-96 absolute ">
         <div className="float-left pl-10 mt-7 pr-10">
           <img src={Logo} alt="Logo" width="130" height="200" />
         </div>
-        <div className="flex gap-20 pl-4 mt-10 ">
-          <div>Buy a Course</div>
-          <div>Community</div>
-          <div>Scholarship</div>
-          <div className="ml-72 ">List your Institute</div>
+        <div
+          className={
+            (showMenu ? "visible " : "invisible md:visible") +
+            " md:flex md:gap-20 md:pl-4 md:mt-10 relative top-20  ml-32 mr-4 md:mr-0  bg-purple-400 z-30  md:top-0 md:bg-opacity-0 rounded-lg  "
+          }
+        >
+          <div className="mb-2 md:mb-0">Buy a Course</div>
+          <div className="mb-2 md:mb-0">Community</div>
+          <div className="mb-2 md:mb-0">Scholarship</div>
+          <div className="md:ml-72 ml-20 mb-2 md:mb-0 ">
+            List your Institute
+          </div>
 
-          <button className="bg-purple-600 text-white pl-3 pr-3 pb-1 rounded-md ">
+          <button className="bg-purple-600 text-white pl-3 pr-3 pb-1 rounded-md ml-24 md:ml-0 mb-2 md:mb-0 ">
             Login
           </button>
         </div>
-        <div className="bg-white relative ml-12 top-32 z-10 pl-20 pr-20 pt-10 rounded-3xl ">
+
+        {showMenu ? (
+          <HiX
+            className=" text-lg   absolute top-10 left-80  lg:hidden"
+            onClick={() => {
+              setShowMenu(!showMenu);
+            }}
+          />
+        ) : (
+          <HiMenu
+            className=" text-lg absolute top-10 left-80 lg:hidden"
+            onClick={() => {
+              setShowMenu(!showMenu);
+            }}
+          />
+        )}
+        <div className="bg-white w-80 md:w-full ml-4     md:ml-12 relative  pl-10 pr-10 pt-10 md:top-32 z-10 md:pl-20 md:pr-20 md:pt-10 rounded-3xl ">
           <div className="">
             <p className="font-semibold">say hi to the team:)</p>
-            <h1 className="font-semibold text-7xl text-purple-600 mb-6 mt-4">
+            <h1 className="font-semibold text-4xl md:text-7xl text-purple-600 mb-6 mt-4">
               Get In Touch
             </h1>
-            <p className="ml-28 font-semibold">
+            <p className="md:ml-28 ml-10 mb-10 md:mb-0 font-semibold">
               feel free to contact us and we will <br /> get back to you as soon
               as we can
             </p>
           </div>
-          <form action="" className="flex flex-col mt-10">
+          <form
+            action=""
+            className="md:flex md:flex-col -ml-8 md:-ml-0 md:mt-10"
+          >
             <label htmlFor="name" className="">
               Your Name
             </label>
             <input
               type="text"
+              onChange={(e) => {
+                setNameValue(e.target.value);
+              }}
               className="bg-purple-100 w-80 h-8 rounded-md  mt-2 mb-2"
             />
+            {name && (
+              <p className="text-red-900 font-semibold">
+                Please Enter valid Name
+              </p>
+            )}
             <label htmlFor="email">Your Email</label>
             <input
+              onChange={(e) => {
+                setEmailValue(e.target.value);
+              }}
               type="email"
               className="bg-purple-100 w-80 h-8 rounded-md   mt-2 mb-2"
             />
+            {email && (
+              <p className="text-red-900 font-semibold">
+                Please Enter valid Email
+              </p>
+            )}
             <label htmlFor="message">Your Message</label>
             <textarea
               name="message"
+              onChange={(e) => {
+                setMessageValue(e.target.value);
+              }}
               placeholder="Type something if you want..."
               className="bg-purple-100 w-80 h-32 rounded-md p-4 mt-2"
             ></textarea>
+            {message && (
+              <p className="text-red-900 font-semibold">
+                Please Enter valid Message
+              </p>
+            )}
           </form>
-          <button className="bg-purple-600 text-white w-32 rounded-lg mt-6 h-8">
+          <button
+            onClick={submitHandler}
+            className="bg-purple-600 text-white w-32 rounded-lg mt-6 mb-10 md:mb-0 h-8"
+          >
             Submit
           </button>
-          <div className=" relative float-right bottom-96 -mt-28 -left-20  ">
-            <div className="bg-gray-400 p-20 w-80 h-52 ">llama animation</div>
-            <div className="flex mt-6">
+          <div className=" relative md:float-right md:bottom-96 md:-mt-28 md:-left-20 top-12 md:top-full  ">
+            <div className="bg-gray-400 -ml-9 md:-ml-0 p-12 md:p-20 w-80 h-52  ">
+              llama animation
+            </div>
+            <div className="flex mt-10 ">
               <HiLocationMarker className="text-purple-600 text-2xl mr-6" />
               <p>
                 Block-A - 1/57 Jangpura Extention <br /> New Delhi - 110014
--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -8,17 +8,23 @@ import { FaLinkedinIn } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer p-0 relative top-52 -ml-28 -mr-20 -pl-1 pr-0   bg-black text-white ">
-      <div className="p-16 pl-32    ">
-        <img src={Logo} alt="Logo" width="130" height="200" className="mb-3" />
-        <div className="flex text-white text-2xl  gap-4 ml-10">
+    <footer className="footer p-0    relative  sm:bottom-10 -bottom-32 md:top-52 -ml-28 -mr-20 -pl-1 pr-0   bg-black text-white ">
+      <div className="p-16 pl-32 mb-72 md:mb-0     ">
+        <img
+          src={Logo}
+          alt="Logo"
+          width="130"
+          height="200"
+          className="md:mb-6 mb-10  absolute md:relative   "
+        />
+        <div className="flex text-white text-2xl absolute top-28 md:top-0 md:relative mt-5  gap-4 ml-10">
           <FaFacebookF />
           <FaInstagramSquare />
           <FaYoutube />
           <FaLinkedinIn />
         </div>
       </div>
-      <div className="pt-16   -mt-48 ml-72 flex ">
+      <div className="md:pt-16  relative -top-40 md:top-0  block md:flex     md:-mt-48 md:ml-72  ">
         <div className=" pl-40">
           <h1 className=" mb-4 flex-initial w-52 ">About us</h1>
           <p className="text-gray-400">Who we are</p>
@@ -26,17 +32,17 @@ const Footer = () => {
           <p className="text-gray-400">Careers</p>
           <p className="text-gray-400">Blogs</p>
         </div>
-        <div className="  w-full">
-          <h1 className="flex-initial mb-4 w-64">Events</h1>
+        <div className=" pl-80 -mt-36 md:-mt-0 md:pl-0 w-full">
+          <h1 className="flex-initial mb-4  w-32 md:w-64">Events</h1>
           <p className="text-gray-400">About Our Events</p>
           <p className="text-gray-400">Ostello's Fest</p>
         </div>
-        <div className="   w-full">
+        <div className=" pl-40 mt-28 md:mt-0 md:pl-0  w-full">
           <h1 className=" flex-initial mb-4 w-64 ">Contact Us</h1>
           <p className="text-gray-400">FAQs</p>
           <p className="text-gray-400">Support</p>
         </div>
-        <div className="  mb-10 flex-initial   ">
+        <div className=" pl-80 -mt-24 md:-mt-0 md:pl-0 mb-10 flex-initial   ">
           <h1 className="mb-4">Terms of Use</h1>
           <p className="flex-initial w-44  text-gray-400">Terms & Conditions</p>
           <p className="text-gray-400">Privacy Policy</p>
@@ -44,7 +50,7 @@ const Footer = () => {
           <p className="text-gray-400">Trust & Safety</p>
         </div>
       </div>
-      <h3 className="text-center ml-96 mt-10 ">
+      <h3 className="text-center ml-10 -mt-32  md:mt-10 md:ml-96   ">
         2022 ©️ Ostello India Private Limited.
       </h3>
     </footer>
