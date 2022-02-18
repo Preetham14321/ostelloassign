1fbded61e944adcc333fa44cff6db1d878673df0
Preetham14321 <79894297+Preetham14321@users.noreply.github.com>
2022-02-18 15:34:27 +0530

    created aboutus and career page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -15,7 +15,7 @@ const AboutUs = () => {
   });
 
   useEffect(() => {
-    setViewPort("");
+    setViewPort(768);
   }, []);
 
   const [showMark, setShowMark] = useState(false);
@@ -53,144 +53,142 @@ const AboutUs = () => {
             : {}
         }
       >
-        <div className="bg-gradient-to-b from-cyan to-white">
-          <NavHeader />
+        <NavHeader />
 
-          <main>
-            {/* Section 1 */}
-            <section
-              className="px-8 lg:px-36 h-fit flex flex-col-reverse lg:flex-row items-center justify-center lg:py-20"
-              // style={{ height: `calc(100vh - 120px)` }}
-            >
-              <div className="first-letter:flex flex-col items-start w-full lg:w-3/5 space-y-6 z-10">
-                <h1 className="text-violet font-bold text-4xl lg:text-5xl">
-                  Our
-                  <span className="text-slate"> Mission</span>
-                </h1>
-                <h3 className="text-ghost text-lg w-full lg:w-3/5">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
-                  nulla illum repudiandae autem quis, consectetur nam non velit
-                  dolores placeat vero tempora laborum alias qui consequuntur
-                  libero officiis maiores blanditiis.
-                </h3>
-              </div>
-              <img
-                src={AboutUsVector}
-                alt=""
-                className=" md:w-3/5 lg:w-2/5 lg:h-auto mb-6 lg:mb-0"
-              />
-            </section>
-            {/* Section 2 */}
-            <div className="bg-gradient-to-b from-white to-cyan">
-              <section className="lg:items-center  px-6 lg:px-36  py-10">
-                <h1 className="text-violet text-center font-bold text-4xl lg:text-5xl mb-10">
-                  Who are <span className="text-slate"> We ?</span>
-                </h1>
-                <div className="flex flex-col   lg:flex-row">
-                  <div className=" m-auto lg:m-0">
-                    <img src={AboutWeOffer} alt="" className=" lg:w-3/4  " />
-                    <h3 className="text-xl font-semibold -mt-5 pl-10">
-                      Connect with mentors
-                    </h3>
-                    <p className="md:w-3/4 pl-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                  <div className=" lg:mt-10 m-auto lg:m-0">
-                    <img src={AboutWeOffer} alt="" className=" lg:w-3/4  " />
-                    <h3 className="text-xl font-semibold -mt-5 pl-10">
-                      Connect with mentors
-                    </h3>
-                    <p className="md:w-3/4 pl-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                  <div className=" lg:mt-20 m-auto lg:m-0">
-                    <img src={AboutWeOffer} alt="" className=" lg:w-3/4  " />
-                    <h3 className="text-xl font-semibold -mt-5 pl-10">
-                      Connect with mentors
-                    </h3>
-                    <p className="md:w-3/4 pl-10">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    </p>
-                  </div>
-                  {/* <div className="lg:mt-10  m-auto lg:m-0">
-                <img src={AboutWeOffer} alt="" className=" md:w-3/4  " />
-                <h3 className="text-xl font-semibold -mt-5 pl-10">
-                  Connect with mentors
-                </h3>
-                <p className="md:w-3/4 pl-10">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-              </div> */}
-                  {/* <div className="mt-20  m-auto lg:m-0">
-                <img src={AboutWeOffer} alt="" className=" md:w-3/4  " />
-                <h3 className="text-xl font-semibold -mt-5 pl-10">
-                  Connect with mentors
-                </h3>
-                <p className="md:w-3/4 pl-10">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                </p>
-              </div> */}
-                </div>
-              </section>
-
-              {/* Section 3 */}
-
-              <section className="lg:items-center  px-6 lg:px-36  py-10">
-                <h1 className="text-violet text-center font-bold text-4xl lg:text-5xl mb-20">
-                  Meet the <span className="text-slate"> team</span>
-                </h1>
-                <div className="">
-                  <div className="flex flex-row space-x-20 ">
-                    <Carousel breakPoints={breakPoints}>
-                      {profiles.map((item) => (
-                        <SliderImage
-                          key={item.id}
-                          src={item.src}
-                          name={item.name}
-                        />
-                      ))}
-                    </Carousel>
-                  </div>
-                </div>
-              </section>
-
-              {/* Section 4 */}
-
-              <section className="py-12">
-                <div className=" h-full flex  flex-wrap  py-12 w-4/5 xl:w-3/5  m-auto lg:rounded-2xl bg-violet ">
-                  <div className="flex flex-col  space-y-2 lg:w-3/5 text-center m-auto lg:m-0 ">
-                    <h3 className="text-white text-lg font-semibold">
-                      Subscribe for our newsletter
-                    </h3>
-                    <p className="text-white text-center  space-y-4  text-sm">
-                      Lorem ipsum dolor sit, hed sit on it.
-                    </p>
-                  </div>
-                  <div className="flex flex-col m-auto lg:m-0  ">
-                    <input
-                      type="email"
-                      className="h-12 mt-4 p-4  rounded-lg md:w-72 xl:w-80"
-                      placeholder="type your email here"
-                    />
-                    <button
-                      onClick={() => {
-                        setShowMark(true);
-                      }}
-                      className=" text-white lg:absolute lg:ml-48 xl:ml-56  rounded-md mt-6 p-1 bg-ghost lg:bg-violet "
-                    >
-                      {showMark ? "✔" : "Subscribe"}
-                    </button>
-                  </div>
-                </div>
-              </section>
+        <main>
+          {/* Section 1 */}
+          <section
+            className="px-8 lg:px-36 h-fit flex flex-col-reverse lg:flex-row items-center justify-center lg:py-20"
+            // style={{ height: `calc(100vh - 120px)` }}
+          >
+            <div className="first-letter:flex flex-col items-start w-full lg:w-3/5 space-y-6 z-10">
+              <h1 className="text-violet font-bold text-4xl lg:text-5xl">
+                Our
+                <span className="text-slate"> Mission</span>
+              </h1>
+              <h3 className="text-ghost text-lg w-full lg:w-3/5">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit
+                nulla illum repudiandae autem quis, consectetur nam non velit
+                dolores placeat vero tempora laborum alias qui consequuntur
+                libero officiis maiores blanditiis.
+              </h3>
             </div>
+            <img
+              src={AboutUsVector}
+              alt=""
+              className=" md:w-3/5 lg:w-2/5 lg:h-auto mb-6 lg:mb-0"
+            />
+          </section>
+          {/* Section 2 */}
+          <div className="bg-gradient-to-b from-white to-cyan">
+            <section className="lg:items-center  px-6 lg:px-36  py-10">
+              <h1 className="text-violet text-center font-bold text-4xl lg:text-5xl mb-10">
+                Who are <span className="text-slate"> We ?</span>
+              </h1>
+              <div className="flex flex-col   lg:flex-row">
+                <div className=" m-auto lg:m-0">
+                  <img src={AboutWeOffer} alt="" className=" lg:w-3/4  " />
+                  <h3 className="text-xl font-semibold -mt-5 pl-10">
+                    Connect with mentors
+                  </h3>
+                  <p className="md:w-3/4 pl-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+                <div className=" lg:mt-10 m-auto lg:m-0">
+                  <img src={AboutWeOffer} alt="" className=" lg:w-3/4  " />
+                  <h3 className="text-xl font-semibold -mt-5 pl-10">
+                    Connect with mentors
+                  </h3>
+                  <p className="md:w-3/4 pl-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+                <div className=" lg:mt-20 m-auto lg:m-0">
+                  <img src={AboutWeOffer} alt="" className=" lg:w-3/4  " />
+                  <h3 className="text-xl font-semibold -mt-5 pl-10">
+                    Connect with mentors
+                  </h3>
+                  <p className="md:w-3/4 pl-10">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+                {/* <div className="lg:mt-10  m-auto lg:m-0">
+                <img src={AboutWeOffer} alt="" className=" md:w-3/4  " />
+                <h3 className="text-xl font-semibold -mt-5 pl-10">
+                  Connect with mentors
+                </h3>
+                <p className="md:w-3/4 pl-10">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div> */}
+                {/* <div className="mt-20  m-auto lg:m-0">
+                <img src={AboutWeOffer} alt="" className=" md:w-3/4  " />
+                <h3 className="text-xl font-semibold -mt-5 pl-10">
+                  Connect with mentors
+                </h3>
+                <p className="md:w-3/4 pl-10">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div> */}
+              </div>
+            </section>
 
-            {/* Main Footer */}
-            <Footer />
-          </main>
-        </div>
+            {/* Section 3 */}
+
+            <section className="lg:items-center  px-6 lg:px-36  py-10">
+              <h1 className="text-violet text-center font-bold text-4xl lg:text-5xl mb-20">
+                Meet the <span className="text-slate"> team</span>
+              </h1>
+              <div className="">
+                <div className="flex flex-row space-x-20 ">
+                  <Carousel breakPoints={breakPoints}>
+                    {profiles.map((item) => (
+                      <SliderImage
+                        key={item.id}
+                        src={item.src}
+                        name={item.name}
+                      />
+                    ))}
+                  </Carousel>
+                </div>
+              </div>
+            </section>
+
+            {/* Section 4 */}
+
+            <section className="py-12">
+              <div className=" h-full flex  flex-wrap  py-12 w-4/5 xl:w-3/5  m-auto lg:rounded-2xl bg-violet ">
+                <div className="flex flex-col  space-y-2 lg:w-3/5 text-center m-auto lg:m-0 ">
+                  <h3 className="text-white text-lg font-semibold">
+                    Subscribe for our newsletter
+                  </h3>
+                  <p className="text-white text-center  space-y-4  text-sm">
+                    Lorem ipsum dolor sit, hed sit on it.
+                  </p>
+                </div>
+                <div className="flex flex-col m-auto lg:m-0  ">
+                  <input
+                    type="email"
+                    className="h-12 mt-4 p-4  rounded-lg md:w-72 xl:w-80"
+                    placeholder="type your email here"
+                  />
+                  <button
+                    onClick={() => {
+                      setShowMark(true);
+                    }}
+                    className=" text-white lg:absolute lg:ml-48 xl:ml-56  rounded-md mt-6 p-1 bg-ghost lg:bg-violet "
+                  >
+                    {showMark ? "✔" : "Subscribe"}
+                  </button>
+                </div>
+              </div>
+            </section>
+          </div>
+
+          {/* Main Footer */}
+          <Footer />
+        </main>
       </div>
     </Fragment>
   );
