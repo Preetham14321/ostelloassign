c94e81a7c25fc6eaa0aa5ab9135ad487b9bb0e72
Preetham14321 <79894297+Preetham14321@users.noreply.github.com>
2022-02-18 15:31:15 +0530

    created aboutus and career page

--- src/pages/Career.js
+++ src/pages/Career.js
@@ -60,41 +60,43 @@ const Career = () => {
             : {}
         }
       >
-        <NavHeader />
+        <div className="bg-gradient-to-b from-cyan to-white">
+          <NavHeader />
 
-        {/* Section 1 */}
-        <section
-          className="px-4 lg:px-36 h-fit flex  items-center justify-center py-20"
-          // style={{ height: `calc(100vh - 120px)` }}
-        >
-          <div className="text-center space-y-4">
-            <h1 className="text-violet text-4xl lg:text-5xl font-semibold ">
-              Explore Careers
-            </h1>
-            <p className=" w-screen">
-              To work with Ostello, you don't need heavy degrees or
-              experience... The only thing you <br /> should bring in is your
-              fun self and a little dedication! Come,hop in on the team!
-            </p>
-          </div>
-        </section>
+          {/* Section 1 */}
+          <section
+            className="px-4 lg:px-36 h-fit flex  items-center justify-center py-20"
+            // style={{ height: `calc(100vh - 120px)` }}
+          >
+            <div className="text-center space-y-4">
+              <h1 className="text-violet text-4xl lg:text-5xl font-semibold ">
+                Explore Careers
+              </h1>
+              <p className=" w-screen">
+                To work with Ostello, you don't need heavy degrees or
+                experience... The only thing you <br /> should bring in is your
+                fun self and a little dedication! Come,hop in on the team!
+              </p>
+            </div>
+          </section>
 
-        {/* Section 2 */}
+          {/* Section 2 */}
 
-        <section>
-          <div className="grid sm:grid-cols-2 lg:grid-cols-3  xl:ml-10 mb-32 ">
-            {jobs.map((item) => (
-              <JobCard
-                key={item.id}
-                category={item.category}
-                jobtype={item.jobtype}
-              />
-            ))}
-          </div>
-        </section>
+          <section>
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3  xl:ml-10 mb-32 ">
+              {jobs.map((item) => (
+                <JobCard
+                  key={item.id}
+                  category={item.category}
+                  jobtype={item.jobtype}
+                />
+              ))}
+            </div>
+          </section>
 
-        {/* Main Footer */}
-        <Footer />
+          {/* Main Footer */}
+          <Footer />
+        </div>
       </div>
     </>
   );
